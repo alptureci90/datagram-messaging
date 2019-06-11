33edf40ce435f6d798d0b663b96f4f6a48f430f1
Diamon <diamon33@gmail.com>
2019-06-11 04:24:30 -0400

    - Refactored Client class. - Added remote host availability check. - Added separate client application.

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,67 +1,159 @@
 package client;
 
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.*;
 import java.net.*;
 
-public class Client {
+public class Client
+{
 
+    private int defaultPort = 1234;
+    private DatagramSocket clientSocket = null;
 
-    public static void main(String[] args){
-         /*DatagramSocket provides a socket to send and receive any datagram packet.
-             It can send even broadcast message if network has that ability.
-             We can manage that by SO_BROADCAST flag which is on by default.
-         */
+    private Pattern connectCommandPattern = Pattern.compile("^(CONNECT\\W*(?<ServerAddress>(\\d{1,3}[.]){3}\\d{1,3}\\b|localhost))$", Pattern.CASE_INSENSITIVE);
+    private Pattern exitCommandPattern = Pattern.compile("^\\W*(EXIT)", Pattern.CASE_INSENSITIVE);
 
-        DatagramSocket sc =null;
+    /**
+     * Starts endless loop of waiting and processing console input.
+     */
+    public void ReadCommands()
+    {
+        Scanner commandScanner = new Scanner(System.in);
+        while (true) {
+            String stringCommand = commandScanner.nextLine();
+            ParseAndExecuteCommand(stringCommand);
+        }
+    }
 
-        try{
-            /*Create DatgramSocket object and bind to any random socket chosen by OS.
-            We can use sc.getLocalPort() to check the chosen port
-             */
-            sc = new DatagramSocket();
+    /**
+     * Creates DatagramSocket on specified address if the remote host is available.
+     *
+     * @param address String address of remote host.
+     */
+    public void ConnectToRemoteServer(String address)
+    {
+        try {
 
-            //Server port number
-            int portNum = 1234;
+            if (clientSocket != null) {
+                System.out.println("Breaking current connection...");
+                clientSocket.close();
+            }
 
-            //Create InetAddress object on localhost
-            InetAddress ad = InetAddress.getByName("localhost");
+            System.out.println("Connecting to " + address);
 
+            if (isReachable(address, defaultPort, 10)) {
+                System.out.println("Host is available, connecting..");
+
+                InetAddress serverAddress = InetAddress.getByName(address);
+                clientSocket = new DatagramSocket();
+                clientSocket.connect(serverAddress, defaultPort);
+
+                System.out.println("Connected to " + serverAddress + ":" + defaultPort);
+
+            } else {
+                System.out.println("Host is unreachable");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Sends input command to remote host and outputs the response.
+     *
+     * @param command Command input.
+     */
+    public void SendServerCommandAndReceiveResponse(String command)
+    {
+        command = command.replace("^\\W*|\\W*$", "");
+        command = command.replace("\\s+", "\t");
+
+        try {
             //Prepare byte array of message
-            byte[] data = "Hello".getBytes();
+            byte[] data = command.getBytes();
 
-              /*Create packet which contains our data and labeled to reach
-            to the port and IP address mentioned
-            */
+            DatagramPacket dataPacket = new DatagramPacket(data, data.length);
 
-            DatagramPacket dp = new DatagramPacket(data, data.length,ad,portNum);
+            clientSocket.send(dataPacket);
 
-            //Send the packet using datagram socket
-            sc.send(dp);
+            byte[] responseData = new byte[1024];
+            DatagramPacket response = new DatagramPacket(responseData, responseData.length);
 
-            //Receive response from sent by process at the other end
-            data = new byte[100];
-            sc.receive(new DatagramPacket(data, data.length));
+            clientSocket.receive(response);
 
-             /*Easy way to print a byte data into string format
-            Ref: //https://www.mkyong.com/java/how-do-convert-byte-array-to-string-in-java/
-             */
-            System.out.println("Message Server: " + new String(dp.getData()));
-            data = "quit".getBytes();
+            System.out.println("Received response: ");
+            System.out.println(new String(response.getData()));
 
-            sc.send(new DatagramPacket(data,data.length,ad,portNum));
-        }
-        catch (SocketException e){
-            //Could be thrown by DatagramSocket
-            System.out.println(e.getMessage());
-        } catch (UnknownHostException e) {
-            //Could be thrown by InetAddress
-            e.printStackTrace();
-        } catch (IOException e) {
-            //Could be thrown while sending/receiving packets
-            e.printStackTrace();
+        } catch (Exception ex) {
+            System.out.println("An error occurred");
+            System.out.println(ex);
         } finally {
-            sc.close();
+            clientSocket.close();
+        }
+    }
+
+    /**
+     * Parses incoming command input and executes it or sends to remote host.
+     * EXIT to exit application.
+     * CONNECT [address] to connect.
+     * Other commands will be sent to remote host.
+     *
+     * @param command Command input.
+     */
+    public void ParseAndExecuteCommand(String command)
+    {
+        try {
+
+            if (exitCommandPattern.matcher(command).matches()) {
+
+                System.out.println("Exiting...");
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+                System.exit(0);
+            }
+
+            Matcher connectCommandMatcher = connectCommandPattern.matcher(command);
+
+            if (connectCommandMatcher.matches()) {
+
+                String serverAddress = connectCommandMatcher.group("ServerAddress");
+                ConnectToRemoteServer(serverAddress);
+            } else {
+
+                if (clientSocket != null) {
+                    System.out.println("Sending command: " + command);
+                    SendServerCommandAndReceiveResponse(command);
+                } else {
+                    System.out.println("Client is not connected.");
+                }
+            }
+        } catch (Exception ex) {
+
+            System.out.println("Hostname not found.");
         }
 
+    }
+
+    /**
+     * Checks if remote host is available by creating a socket with specified address and port.
+     *
+     * @param address       Remote host address.
+     * @param openPort      Remote host port.
+     * @param timeOutMillis Socket timeout.
+     * @return
+     */
+    private boolean isReachable(String address, int openPort, int timeOutMillis)
+    {
+        try {
+            try (DatagramSocket soc = new DatagramSocket()) {
+                soc.connect(new InetSocketAddress(address, openPort));
+            }
+            return true;
+        } catch (IOException ex) {
+            return false;
+        }
     }
 }
---
+++ src/main/java/client/ClientApplication.java
@@ -0,0 +1,11 @@
+package client;
+
+public class ClientApplication
+{
+    public static void main(String[] args)
+    {
+        Client client = new Client();
+        client.ReadCommands();
+    }
+
+}
